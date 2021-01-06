ee6d70efe13a6aeda62d08ae9179c6d2b5f1634e
Marc Hollander <marc.hollander@oneshoe.nl>
2021-01-06 11:12:02 +0100

    Improve common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Bash colors and formatting
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
 function log() {
-  echo -e "\033[34m[$(date +'%F %T')] \033[1m$1\033[0m"
+  echo -e "\033[34m[$(date +'%F %T')]\033[0m $1\033[0m"
 }
 
 function log_error() {
@@ -10,6 +13,10 @@ function log_error() {
 
 function log_success() {
     echo -e "\033[32m$1\033[0m"
+}
+
+function log_text() {
+    echo -e "$1\033[0m"
 }
 
 function log_warning() {
