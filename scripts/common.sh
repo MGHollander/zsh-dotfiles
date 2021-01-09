d286d454e9fa9067497a07a7b3944c3f7282c737
Marc Hollander <marc.hollander@oneshoe.nl>
2021-01-09 16:16:54 +0100

    Add size calculation function to common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -22,3 +22,15 @@ function log_text() {
 function log_warning() {
     echo -e "\033[33m$1\033[0m"
 }
+
+# Converts bytes value to human-readable string [$1: bytes value]
+# Source: https://unix.stackexchange.com/a/259254
+function bytesToHumanReadable() {
+    local i=${1:-0} d="" s=0 S=("Bytes" "KiB" "MiB" "GiB" "TiB" "PiB" "EiB" "YiB" "ZiB")
+    while ((i > 1024 && s < ${#S[@]}-1)); do
+        printf -v d ".%02d" $((i % 1024 * 100 / 1024))
+        i=$((i / 1024))
+        s=$((s + 1))
+    done
+    echo "$i$d ${S[$s]}"
+}
