d0d2767497a2559a63d777fb639117bcf5cbd915
Marc Hollander <marc.hollander@oneshoe.nl>
2021-01-27 16:10:42 +0100

    Small improvement in script confirmations

--- scripts/mysql/mysql-import.sh
+++ scripts/mysql/mysql-import.sh
@@ -113,7 +113,7 @@ log "\033[1mDelete and re-create database"
 
 if [ -z "$FORCE" ]; then
     log_warning "Are you sure you want to overwrite \033[4;33m$DB_NAME\033[0;33m? This cannot be undone!"
-    read -rp "Continue? [Y/n] " -n 1
+    read -rp "Continue? [y/N] " -n 1
     echo # move to a new line
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         log_error "Imported aborted"
