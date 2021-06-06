729f728bfec7eaa17513235b1d4bfc9f84629c5c
Marc Hollander <marc.hollander@oneshoe.nl>
2021-06-06 21:20:26 +0200

    Fix some shellcheck errors and check for drush in mydrush

--- scripts/mydrush.sh
+++ scripts/mydrush.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC1091
 source "$(dirname "$0")/common.sh"
 
 COMMAND="drush"
@@ -11,12 +12,15 @@ fi
 
 if [ "$1" == "uli" ]; then
     COMMAND="$COMMAND --no-browser"
+    PROJECT_NAME=$(basename "$PWD")
 
-    PROJECT_NAME=`basename $PWD`
-
-    # The valet domain name can be detected automatically, but than you need do type the root password. I don't want to do that everytime...
+    # The valet domain name can be detected automatically, but then you need to
+    # type the root password. I don't want to do that everytime...
     COMMAND="$COMMAND --uri=$PROJECT_NAME.valet"
 fi
 
-# @TODO Check if drush is installed
-$COMMAND $@
+if hash drush 2>/dev/null; then
+    $COMMAND "$@"
+else
+    log_error "Could not find drush"
+fi
