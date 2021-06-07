4c9b80c29bbba7d0ab48d2ea7ddc6c2224717b6a
Marc Hollander <marc.hollander@oneshoe.nl>
2021-06-07 22:33:55 +0200

    Refactor create-project.sh

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -5,9 +5,12 @@ set -e
 
 source "$(dirname "$0")/../.common"
 
-# Test command:
-# rm -rf dotfiles && bash ~/dotfiles/scripts/create-project.sh git@gitlab.com:MGHollander/dotfiles.git --no-database
-# rm -rf drupal8-training && bash ~/dotfiles/scripts/create-project.sh git@gitlab.com:MGHollander/drupal8-training.git -b 13-website-header --no-database
+# Set default variables
+MYSQL_HOST=${MYSQL_HOST:-localhost}
+MYSQL_USER=${MYSQL_USER:-root}
+MYSQL_PASS=${MYSQL_PASS:-root}
+MYSQL_CHAR_SET=${MYSQL_CHAR_SET:-utf8mb4}
+MYSQL_COLLATE=${MYSQL_COLLATE:-utf8mb4_unicode_ci}
 
 # TODO:
 # - Add posibility to add files to the Drupal files folder
@@ -35,8 +38,8 @@ while (( "$#" )); do
     case "$1" in
         -h|--help)
             usage
-            exit 0
-        ;;
+            exit
+            ;;
         -b|--branch)
             BRANCH=$2
             shift 2
@@ -61,8 +64,8 @@ while (( "$#" )); do
             shift
             break
             ;;
-        -*|--*=) # unsupported flags
-            log_error "Error: Unsupported flag $1" >&2
+        -*) # unsupported flags
+            log_error "Error: Unsupported flag \033[1m$1" >&2
             echo ""
             usage
             exit 1
@@ -77,65 +80,46 @@ done
 eval set -- "$PARAMS"
 
 GIT_REPO=$1
-
-if [ -z $GIT_REPO ]; then
+if [ -z "$GIT_REPO" ]; then
     log_error "Please provide a Git repository"
     echo ""
     usage
-    exit 2
+    exit 1
 fi
 
 # TODO
 #  - Add a check if a project name already exists to prevent that two projects are cloned in the same folder.
 #  - Add an option to replace the existing folder by the one that you want to create.
-if [ -z $PROJECT_NAME ]; then
-    PROJECT_NAME=`echo $GIT_REPO | sed -n 's#.*/\([^.]*\)\.git#\1#p'`
-fi
-
-BRANCH_COMMAND=""
-if [ $BRANCH ]; then
-    BRANCH_COMMAND="-b $BRANCH"
+if [ -z "$PROJECT_NAME" ]; then
+    PROJECT_NAME=$(echo "$GIT_REPO" | sed -n 's#.*/\([^.]*\)\.git#\1#p')
 fi
 
 # TODO Make the checkout optional?
 # TODO Add check to see if git is installed
 log "Checkout project"
-git clone $GIT_REPO $PROJECT_NAME $BRANCH_COMMAND
-
-if [ $? -gt 0 ]; then
-    exit 4
+if [ -n "$BRANCH" ]; then
+    git clone "$GIT_REPO" "$PROJECT_NAME" -b "$BRANCH" || exit 1
+else
+    git clone "$GIT_REPO" "$PROJECT_NAME" || exit 1
 fi
 log_success "Project cloned succesfully"
 
-# TEST TEST TEST
-bash "$SCRIPT_DIR/php-version-check.sh"
-
 # TODO Check if database exists and ask if it should be replaced
 # TODO Add possibility to import a database
-if [ -z $NO_DATABASE ]; then
-    if [ -z $DATABASE_NAME ]; then
+if [ -z "$NO_DATABASE" ]; then
+    if [ -z "$DATABASE_NAME" ]; then
         DATABASE_NAME=$PROJECT_NAME
     fi
 
-    log "Check database access"
-    command -v mysqladmin > /dev/null || { log_error "No mysqladmin found, install a database first."; exit 1; }
-
-    if [ -z $MYSQL_PASS ]; then
-        DATABASE_PASSWORD=""
-        mysqladmin -u ${MYSQL_USER} --skip-password status || { log_error "Could not reach database."; exit 1; }
-    else
-        DATABASE_PASSWORD="--password=${MYSQL_PASS}"
-        mysqladmin -u ${MYSQL_USER} ${DATABASE_PASSWORD} status || { log_error "Could not reach database."; exit 1; }
-    fi
-
     log "Create a MySQL database if needed"
-    mysql -u ${MYSQL_USER} ${DATABASE_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" || exit 1
-    mysql -u ${MYSQL_USER} ${DATABASE_PASSWORD} -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@localhost IDENTIFIED BY '${MYSQL_PASS}';" || exit 1
-    mysql -u ${MYSQL_USER} ${DATABASE_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASS}';" || exit 1
+    mysql_create_db "$DATABASE_NAME"
 fi
 
 log "Go to project root folder"
-cd $PROJECT_NAME/
+cd "$PROJECT_NAME/"
+
+# TODO TEST TEST TEST
+bash "$SCRIPT_DIR/php-version-check.sh"
 
 # The build script needs to run first, because Drupal 8 and Drupal 7 with a make
 # file scaffold files like the default.setting.php. And we need those to
@@ -148,27 +132,20 @@ else
 fi
 
 log "Look for the webroot folder"
-
 WEBROOT=$(getWebRoot)
 if [ -z "$WEBROOT" ]; then
+    # TODO add checks to determine the CMS / framework and add a settings file for other tools
     if [ -d "./sites" ]; then
         WEBROOT="."
     fi
 fi
 
-if [ -z $WEBROOT ]; then
-    log_error "Couldn't find the webroot folder"
-    exit 5
+if [ -n "$WEBROOT" ]; then
+    # TODO add checks to determine the CMS / framework and add a settings file for other tools
+    bash "$SCRIPT_DIR/drupal-create-settings-file.sh" "$WEBROOT" "$PROJECT_NAME"
+    bash "$SCRIPT_DIR/drupal-copy-local-settings-file.sh" "$WEBROOT"
 fi
 
-# TODO add checks to determine the CMS / framework and add a settings file for other tools
-bash $SCRIPT_DIR/drupal-create-settings-file.sh $WEBROOT $PROJECT_NAME
-
-if [ $? -gt 0 ]; then
-    exit 6$?
-fi
-
-bash $SCRIPT_DIR/drupal-copy-local-settings-file.sh $WEBROOT
 
 # TODO make clean install optional
 # TODO add a command to import a db from a file instead of running a clean install
@@ -184,15 +161,14 @@ fi
 if [ -z $NO_LINK ]; then
     log "Link project to valet"
 
-    cd $WEBROOT || exit 1
-    valet link $PROJECT_NAME
+    cd "$WEBROOT" || exit 1
+    valet link "$PROJECT_NAME"
     cd - || exit 1
 
-    VALET_DOMAIN=`valet tld`
-    PROJECT_URL="http://${PROJECT_NAME}.${VALET_DOMAIN}"
+    PROJECT_URL="http://$PROJECT_NAME.$(valet tld)"
 
     log_success "Project link set successfully"
-    echo "You can visit the project at ${PROJECT_URL}"
+    echo "You can visit the project at $PROJECT_URL"
 fi
 
 log "Project creation finished"
