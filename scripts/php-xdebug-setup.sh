ef176dd601ad94b290b758fbc4637243fd4fdea8
Marc Hollander <marc.hollander@oneshoe.nl>
2021-06-18 12:33:03 +0200

    Add php-xdebug-setup.sh for future feature

---
+++ scripts/php-xdebug-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Stop on any error.
+set -e
+
+source "$(dirname "$0")/../.common"
+
+# TODO build the script
+log "One day, this script will automaticcally setup xdebug for the active PHP version"
